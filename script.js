6c58d4887d2dc7b5d0c843ac17108958508dd539
harshavardhan314 <hharshavardhan974@gmail.com>
2025-06-28 17:27:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-//displaying output
-window.alert("hi");
-console.log("harsha");
-document.getElementById("h1").textContent="hello";
 
-// variables decalaration
-
- let a=10;
- let b=2;
- let ok=true;
-window.alert( `hey i am currently doing javascript ?${ok} ` );
-
-console.log(Math.pow(a,b));
 
