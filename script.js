417b9ba27430b72ae45c43110ea4044a543d5437
harshavardhan314 <hharshavardhan974@gmail.com>
2025-06-28 17:07:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ document.getElementById("h1").textContent="hello";
 // variables decalaration
 
  let a=10;
- let b=20;
+ let b=2;
  let ok=true;
 window.alert( `hey i am currently doing javascript ?${ok} ` );
 
+console.log(Math.pow(a,b));
 
