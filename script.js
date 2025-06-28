b6b2516047f57bc515a1d90da60a8aa708a2b933
harshavardhan314 <hharshavardhan974@gmail.com>
2025-06-28 17:46:33 +0530

    taking input from user

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-
-
+let username;
+document.getElementById("submit").onclick=function()
+{
+    username=document.getElementById("input").value;
+    document.getElementById("h1").textContent="welcome harsha";
+}
