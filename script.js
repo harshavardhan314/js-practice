5789207a7ab727bb4b1fc462b98bb4d2b8de6882
harshavardhan314 <hharshavardhan974@gmail.com>
2025-06-28 16:13:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-
-//comments
 //displaying output
-
 window.alert("hi");
-
 console.log("harsha");
-
 document.getElementById("h1").textContent="hello";
+
+// variables decalaration
+
+ a=10;
+ b=20;
+
+ ans=a+b;
+window.alert( `sum= ${ans} ` );
+
 
