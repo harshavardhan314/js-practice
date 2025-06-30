70f87fc222b7adc3ceba439e5305ca8694e9ced6
harshavardhan314 <hharshavardhan974@gmail.com>
2025-06-30 11:36:14 +0530

    conversions

--- script.js
+++
@@ -1,6 +0,0 @@
-let username;
-document.getElementById("submit").onclick=function()
-{
-    username=document.getElementById("input").value;
-    document.getElementById("h1").textContent="welcome harsha";
-}hy 
