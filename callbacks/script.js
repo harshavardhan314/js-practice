9230b2445d88962b7ce3cc0d18f5ec016ce27ae6
harshavardhan314 <hharshavardhan974@gmail.com>
2025-07-05 22:50:54 +0530

    Update script.js

--- callbacks/script.js
+++ callbacks/script.js
@@ -1,6 +1,42 @@
-person={
-    name:"harsha",
-    age:15,
-    sayhello:()=>console.log(`hello ${this.name}`)
+class rectangle
+{
+    constructor(len,ber)
+    {
+        this.len=len
+        this.ber=ber
+    }
+    set len(len)
+    {
+        if(len>0)
+        {
+            this._len=len
+        }
+        else{
+            console.log("invalid number")
+        }
+
+    }
+    set ber(ber)
+    {
+        if(ber>0)
+        {
+            this._ber=ber
+        }
+        else{
+            console.log("invalid number")
+        }
+
+    }
+    get len(){
+    return this._len;
+    }
+    get ber()
+    {
+        return this._ber;
+    }
+
+    
 }
-console.log(person.sayhello())
+let obj=new rectangle(1,3)
+console.log(obj.len)
+console.log(obj.ber)
